1d81685c612918b7e2b78061c2f6bc1e7927cb3a
Pt4r <palaiologosstr@hotmail.com>
2019-11-14 15:00:40 +0200

    Created Person Class

---
+++ Student.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Demo_Website
+{
+    public class Student{
+        public in Id {get;set;}
+        public string FirstName{get;set;}
+        public string LastName{get;set;}
+    }
+}
